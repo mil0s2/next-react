526a7ad3bc353e14339706e78a2e6502c0978bc6
Milosz Mazurkiewicz <miloszmazurk@gmail.com>
2021-08-28 13:22:52 +0200

    fixed a typo

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,25 +1,23 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb';
 
 // /api/new-meetup
 // POST requests only trigger this code due to the line 4
 async function handler(req, res) {
-  if (req.method === "POST") {
+  if (req.method === 'POST') {
     const data = req.body;
     console.log(data);
     const client = await MongoClient.connect(
-      "mongodb+srv://milosz:1234@cluster0.yvtko.mongodb.net/meetups?retryWrites=true&w=majority"
+      'mongodb+srv://milosz:1234@cluster0.yvtko.mongodb.net/meetups?retryWrites=true&w=majority'
     );
     const db = client.db();
-    const meetupsCollection = db.collection("meetups");
+    const meetupsCollection = db.collection('meetups');
 
     const result = await meetupsCollection.insertOne({ data });
     console.log(result);
 
     client.close();
 
-    res.status(201).js;
-
-    on({ message: "Meetup inserted!" });
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
